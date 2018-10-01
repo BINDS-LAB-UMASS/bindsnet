757b2045c0bc4edb040595b84f7f9c658f72fb15
devdhar <devdharpatel@gmail.com>
2018-09-30 21:37:22 -0400

    Fix bug in IF node

--- bindsnet/network/nodes.py
+++ bindsnet/network/nodes.py
@@ -196,7 +196,7 @@ class IFNodes(Nodes):
         Resets relevant state variables.
         '''
         super()._reset()
-        self.v = self.rest * torch.ones(self.shape)  # Neuron voltages.
+        self.v = self.reset * torch.ones(self.shape)  # Neuron voltages.
         self.refrac_count = torch.zeros(self.shape)  # Refractory period counters.
 
 
