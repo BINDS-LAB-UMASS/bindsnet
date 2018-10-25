d4ebc7f73c32d11a10bd1997ca5c767caa5c025d
devdhar <devdharpatel@gmail.com>
2018-10-25 13:06:34 -0400

    Add parameter search folder.

---
+++ parameter_search/submit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=probabilistic_param_search
+#SBATCH --partition=1080ti-long
+#SBATCH --time=01-18:00:00
+#SBATCH --mem=8000
+#SBATCH --account=rkozma
+#SBATCH --output=res_%j.txt
+#SBATCH -e res_%j.err
+#SBATCH --gres=gpu:1
+
+layer1scale=${1:-5}
+layer2scale=${2:-1}
+
+echo $layer1scale $layer2scale $occlusionloc
+
+python3 dqn_playground_spiking.py --layer1scale $layer1scale --layer2scale $layer2scale
+exit
